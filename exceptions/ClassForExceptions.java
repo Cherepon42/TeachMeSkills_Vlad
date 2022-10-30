2a91f25eb75d2fa67592825225777ec3b54a9f5f
Vlad <UladCherap@yandex.by>
2022-10-30 16:10:10 +0300

    Initial HW_10,11

--- exceptions/ClassForExceptions.java
+++ exceptions/ClassForExceptions.java
@@ -1,7 +1,5 @@
 package exceptions;
 
-import javax.swing.*;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
--- strings/Strings_3.java
+++ strings/Strings_3.java
@@ -1,7 +1,5 @@
 package strings;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Strings_3 {
